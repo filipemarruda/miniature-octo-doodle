85c1a3c845fd4568ebc5adabe615a65d3173dc95
Filipe Mendes Arruda <filipe.arruda@sysmap.com.br>
2023-10-04 22:52:37 -0300

    feat(#001): creates tests for the insert and select all db methods

--- __test__/src/modules/posts/data/db.test.js
+++ __test__/src/modules/posts/data/db.test.js
@@ -8,3 +8,25 @@ test('create tables does not send any error', async () => {
     expect(error).toBeNull();
   }
 });
+
+
+test('insert post do not send any error', async () => {
+  try {
+    await fixture.insert({
+      userId: 1,
+      id: 1,
+      title: "title",
+      body: "body"
+    });
+  } catch (error) {
+    expect(error).toBeNull();
+  }
+});
+
+test('select all posts do not send any error', async () => {
+  try {
+    await fixture.selectAll();
+  } catch (error) {
+    expect(error).toBeNull();
+  }
+});
