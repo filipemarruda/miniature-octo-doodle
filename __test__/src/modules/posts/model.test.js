a82363501773c8c8875fc425008978dca81e50bd
Filipe Mendes Arruda <filipe.arruda@sysmap.com.br>
2023-10-04 22:58:02 -0300

    feat(#001): create save all posts simple test

--- __test__/src/modules/posts/model.test.js
+++ __test__/src/modules/posts/model.test.js
@@ -8,3 +8,16 @@ test('get all posts does not send any error', async () => {
     expect(error).toBeNull();
   }
 });
+
+test('save all posts does not send any error', async () => {
+  try {
+    await fixture.saveAll([{
+      userId: 1,
+      id: 1,
+      title: "title",
+      body: "body"
+    }]);
+  } catch (error) {
+    expect(error).toBeNull();
+  }
+});
