df8b3b0d292a4661dae04a41e4e8d053405fefcd
Filipe Mendes Arruda <filipe.arruda@sysmap.com.br>
2023-10-04 23:03:39 -0300

    feat(#001): create users map by user id

--- src/modules/posts/service.js
+++ src/modules/posts/service.js
@@ -1,4 +1,13 @@
+const api = require('./data/api');
 const model = require('./model');
+
+const _getUserMap = async () => {
+  return (await api.fetchUsers()).reduce((acc, user) => {
+    acc[user.id] = user;
+    return acc;
+  }, {});
+}
+
 
 const findAll = async () => {
   let posts = await model.getAll();
